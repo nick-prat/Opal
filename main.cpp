7d278c2bb8fb78d2119292268d47b9ad35df3bcf
Nick Prat <nprat@uwo.ca>
2016-05-15 22:11:01 -0400

    stuff

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-#include "main.h"
+#include <GL/gl.h>
+#include <iostream>
+#include <string>
+#include "Display/Display.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Lol literally nothing" << endl;
+    Display* display = new Display();
+    if(!display->InitDisplay(800, 600, "First OpenGL Project"))
+    {
+        cerr << "Couldn't start display" << endl;
+        return 1;
+    }
+
+    cout << "Display address: " << display << endl;
+
+    while(!display->IsClosed())
+    {
+        glClearColor(0.5f, 0.25f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        display->Update();
+    }
 
     return 0;
 }
