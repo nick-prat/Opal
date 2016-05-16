db339f088570c4272c6a4d19b4a077a5c3e4a8a0
Nick Prat <nprat@uwo.ca>
2016-05-15 22:54:52 -0400

    stuff

---
+++ Display/Display.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <GL/glew.h>
+#include "Display.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+Display::Display()
+{
+    m_window = nullptr;
+}
+
+
+Display::~Display()
+{
+
+}
+
+bool Display::InitDisplay(int width, int height, std::string title)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    m_window = SDL_CreateWindow(title.c_str(), 0, 0, width, height, SDL_WINDOW_OPENGL);
+    m_glcontext = SDL_GL_CreateContext(m_window);
+
+    GLenum status = glewInit();
+
+    if(status != GLEW_OK)
+    {
+        cerr << "Glew failed to initialize" << endl;
+        return false;
+    }
+
+    m_isClosed = false;
+
+    return true;
+}
+
+void Display::Destroy()
+{
+    SDL_GL_DeleteContext(m_glcontext);
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+}
+
+void Display::Update()
+{
+    SDL_GL_SwapWindow(m_window);
+
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            m_isClosed = true;
+        }
+    }
+}
+
+bool Display::IsClosed()
+{
+    return m_isClosed;
+}
+
---
+++ Display/Display.h
@@ -0,0 +1,26 @@
+#ifndef _DISPLAY_H
+#define _DISPLAY_H
+
+#include <string>
+#include <SDL2/SDL.h>
+
+class Display
+{
+
+public:
+    Display();
+    ~Display();
+
+    bool InitDisplay(int width, int height, std::string title);
+    void Destroy();
+
+    void Update();
+    bool IsClosed();
+
+private:
+    SDL_Window* m_window;
+    SDL_GLContext m_glcontext;
+    bool m_isClosed;
+};
+
+#endif // _DISPLAY_H
