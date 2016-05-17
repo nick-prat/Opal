742b577f54ae60d979a7323c81c2f836dbebc68f
Nick Prat <npissoawsome@gmail.com>
2016-05-17 00:43:01 -0400

    Added shader files

---
+++ Shader/shader.h
@@ -0,0 +1,18 @@
+#ifndef _SHADER_H
+#define _SHADER_H
+
+
+class Shader
+{
+public:
+    Shader();
+    ~Shader();
+
+    bool InitShader();
+    void Destroy();
+
+private:
+
+};
+
+#endif // _SHADER_H
