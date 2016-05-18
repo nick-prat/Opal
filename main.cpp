8aed02da741d8637a4b81bc914540376ef0f9902
Nick Prat <npissoawsome@gmail.com>
2016-05-17 21:01:19 -0400

    editing main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <GL/gl.h>
 #include <iostream>
+#include <string>
 
 #include "display.h"
 
@@ -7,7 +9,14 @@ int main(int argc, char **argv)
 	Display* display = new Display();
 	display->InitDisplay(500, 500, "OpenGL Game");
 	
+	RenderChain* renderChain = new RenderChain();
 	
+	while(!display->IsClosed())
+    {
+        glClearColor(0.5f, 0.25f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        display->Update();
+    }
 	
 	display->Destroy();
 	return 0;
