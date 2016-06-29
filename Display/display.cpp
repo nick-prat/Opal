0e1562611dc11cbc422685059bd2dab665e95047
Nick Prat <npissoawsome@gmail.com>
2016-06-28 21:19:35 -0400

    Tabbing with four spaces

--- Display/display.cpp
+++ Display/display.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <glm/glm.hpp>
@@ -13,10 +12,10 @@ using Utilities::SafeDelete;
 Display::Display()
 {
     m_window = nullptr;
-    m_glcontext = nullptr;
+    m_glContext = nullptr;
     m_inputModule = nullptr;
-	m_isClosed = false;
-	m_projMatrix = glm::mat4(1.0f);
+    m_isClosed = false;
+    m_projMatrix = glm::mat4(1.0f);
 }
 
 Display::Display(int width, int height, std::string title)
@@ -29,7 +28,6 @@ Display::Display(int width, int height, std::string title)
         std::cout << "Couldn't init display" << std::endl;
         throw;
     }
-
 }
 
 Display::~Display()
@@ -39,15 +37,16 @@ Display::~Display()
 
 glm::mat4 Display::GetProjectionMatrix()
 {
-	return m_projMatrix;
+    return m_projMatrix;
 }
 
 bool Display::InitDisplay(int width, int height, std::string title)
 {
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		std::cout << "Couldn't initialize SDL: " << SDL_GetError() << std::endl;
-	}
+    {
+        std::cout << "Couldn't initialize SDL: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
@@ -55,13 +54,26 @@ bool Display::InitDisplay(int width, int height, std::string title)
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+#ifdef  VERSION_MIN
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	
+#endif
+
     m_window = SDL_CreateWindow(title.c_str(), 0, 0, width, height, SDL_WINDOW_OPENGL);
-	
-    m_glcontext = SDL_GL_CreateContext(m_window);
+    if(m_window == nullptr)
+    {
+        std::cout << "Couldn't create SDL window" << std::endl;
+        return false;
+    }
+
+    m_glContext = SDL_GL_CreateContext(m_window);
+    if(m_glContext == nullptr)
+    {
+        std::cout << "Couldn't create gl context" << std::endl;
+        return false;
+    }
 
     glewExperimental = (GLboolean) true;
     GLenum status = glewInit();
@@ -73,7 +85,6 @@ bool Display::InitDisplay(int width, int height, std::string title)
     }
 
     m_projMatrix = glm::perspective(glm::radians(75.0f), (float) width / (float) height, 0.1f, 100.0f);
-
     m_inputModule = std::make_shared<InputModule>();
     m_cameraModule = std::make_shared<CameraModule>();
 
@@ -82,7 +93,7 @@ bool Display::InitDisplay(int width, int height, std::string title)
 
 void Display::Destroy()
 {
-    SDL_GL_DeleteContext(m_glcontext);
+    SDL_GL_DeleteContext(m_glContext);
     SDL_DestroyWindow(m_window);
     SDL_Quit();
 }
@@ -90,25 +101,25 @@ void Display::Destroy()
 void Display::Update()
 {
     SDL_GL_SwapWindow(m_window);
-	
+
     SDL_Event event;
 
     while(SDL_PollEvent(&event))
     {
-		switch(event.type)
-		{
-		case SDL_QUIT:
-			m_isClosed = true;
-			break;
-		case SDL_KEYDOWN:
+        switch(event.type)
+        {
+        case SDL_QUIT:
+            m_isClosed = true;
+            break;
+        case SDL_KEYDOWN:
             m_inputModule->UpdateKey(event.key.keysym.sym, true);
-			break;
-		case SDL_KEYUP:
+            break;
+        case SDL_KEYUP:
             m_inputModule->UpdateKey(event.key.keysym.sym, false);
-			break;
+            break;
         default:
             break;
-		}
+        }
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -20,157 +20,159 @@ public:
     ShittyObject()
     {
         m_VBO = 0;
-		m_IBO = 0;
+        m_IBO = 0;
     }
 
-	ShittyObject(std::shared_ptr<Display> display)
-	{
-		if(!InitObject(display))
-		{
-			std::cout << "Couldn't init shitty model" << std::endl;
-			throw;
-		}
-	}
+    ShittyObject(std::shared_ptr<Display> display)
+    {
+        if(!InitObject(display))
+        {
+            std::cout << "Couldn't init shitty model" << std::endl;
+            throw;
+        }
+    }
 
-	~ShittyObject()
-	{
-		Destroy();
-	}
+    ~ShittyObject()
+    {
+        Destroy();
+    }
 
-	bool InitObject(std::shared_ptr<Display> display)
-	{
-		m_display = display;
+    bool InitObject(std::shared_ptr<Display> display)
+    {
+        m_display = display;
 
-		m_verts.reserve(4);
-		m_verts[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
-		m_verts[1] = glm::vec3(1.0f, -1.0f, 0.0f);
-		m_verts[2] = glm::vec3(-1.0f, 1.0f, 0.0f);
-		m_verts[3] = glm::vec3(1.0f, 1.0f, 0.0f);
+        m_verts.reserve(4);
+        m_verts[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+        m_verts[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+        m_verts[2] = glm::vec3(-1.0f, 1.0f, 0.0f);
+        m_verts[3] = glm::vec3(1.0f, 1.0f, 0.0f);
 
-		glGenBuffers(1, &m_VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * 4, m_verts.data(), GL_STATIC_DRAW);
+        glGenBuffers(1, &m_VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * 4, m_verts.data(), GL_STATIC_DRAW);
 
-		m_indices.reserve(6);
-		m_indices[0] = 0;
-		m_indices[1] = 1;
-		m_indices[2] = 2;
-		m_indices[3] = 1;
-		m_indices[4] = 3;
-		m_indices[5] = 2;
+        m_indices.reserve(6);
+        m_indices[0] = 0;
+        m_indices[1] = 1;
+        m_indices[2] = 2;
+        m_indices[3] = 1;
+        m_indices[4] = 3;
+        m_indices[5] = 2;
 
-		glGenBuffers(1, &m_IBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 6, m_indices.data(), GL_STATIC_DRAW);
+        glGenBuffers(1, &m_IBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 6, m_indices.data(), GL_STATIC_DRAW);
 
-		m_shader = std::make_unique<Shader>();
-		//std::vector<std::string> files = {"Shaders/shader.vs", "Shaders/shader.fs"};
-		//std::vector<GLenum> types = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
-		std::vector<std::string> files = {"Shaders/shader.vs", "Shaders/shader.fs", "Shaders/shader.gs"};
-		std::vector<GLenum> types = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_GEOMETRY_SHADER};
-		if(!m_shader->InitShader(files, types))
-		{
-			std::cout << "Couldn't initialize shader" << std::endl;
-			return false;
-		}
+        m_shader = std::make_unique<Shader>();
+        //std::vector<std::string> files = {"Shaders/shader.vs", "Shaders/shader.fs"};
+        //std::vector<GLenum> types = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
+        std::vector<std::string> files = {"Shaders/shader.vs", "Shaders/shader.fs", "Shaders/shader.gs"};
+        std::vector<GLenum> types = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_GEOMETRY_SHADER};
+        if(!m_shader->InitShader(files, types))
+        {
+            std::cout << "Couldn't initialize shader" << std::endl;
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	void Destroy()
-	{
-		glDeleteBuffers(1, &m_VBO);
-		m_VBO = 0;
-		glDeleteBuffers(1, &m_IBO);
-		m_IBO = 0;
-	}
+    void Destroy()
+    {
+        glDeleteBuffers(1, &m_VBO);
+        m_VBO = 0;
+        glDeleteBuffers(1, &m_IBO);
+        m_IBO = 0;
+    }
 
-	void Render()
-	{
-		m_shader->UseShader();
+    void Render()
+    {
+        m_shader->UseShader();
 
-		glm::mat4 mvp = m_display->GetProjectionMatrix() * m_display->GetCameraModule()->GetViewMatrix() * GetWorld();
+        glm::mat4 mvp = m_display->GetProjectionMatrix() * m_display->GetCameraModule()->GetViewMatrix() * GetWorld();
 
-		GLint worldLocation = glGetUniformLocation(m_shader->GetProgram(), "gMVP");
-		if(worldLocation == -1)
-		{
-			std::cout << "Couldn't get uniform loaction" << std::endl;
-		}
-		glUniformMatrix4fv(worldLocation, 1, GL_FALSE, glm::value_ptr(mvp));
+        GLint worldLocation = glGetUniformLocation(m_shader->GetProgram(), "gMVP");
+        if(worldLocation == -1)
+        {
+            std::cout << "Couldn't get uniform loaction" << std::endl;
+        }
+        glUniformMatrix4fv(worldLocation, 1, GL_FALSE, glm::value_ptr(mvp));
 
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDrawArrays(GL_POINTS, 0, 4);
+        glDrawArrays(GL_POINTS, 0, 4);
 
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-		glDisableVertexAttribArray(0);
-	}
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glDisableVertexAttribArray(0);
+    }
 
 private:
     GLuint m_VBO;
-	GLuint m_IBO;
-	std::vector<glm::vec3> m_verts;
-	std::vector<uint> m_indices;
-	std::unique_ptr<Shader> m_shader;
+    GLuint m_IBO;
+    std::vector<glm::vec3> m_verts;
+    std::vector<uint> m_indices;
+    std::unique_ptr<Shader> m_shader;
 };
 
 int main(int argc, char **argv)
 {
-	auto display = std::make_shared<Display>(1280, 720, "OpenGL Game");
-	auto renderChain = std::make_shared<RenderChain>(10);
-	auto obj = std::make_shared<ShittyObject>(display);
+    auto display = std::make_shared<Display>(1280, 720, "OpenGL Game");
+    auto renderChain = std::make_shared<RenderChain>(10);
+    auto obj = std::make_shared<ShittyObject>(display);
 
-	//obj->Scale(glm::scale(glm::vec3(0.5f, 1.0f, 1.0f)));
-	//obj->Rotate(glm::rotate(glm::pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f)));
-	//obj->Translate(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+    //obj->Scale(glm::scale(glm::vec3(0.5f, 1.0f, 1.0f)));
+    //obj->Rotate(glm::rotate(glm::pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f)));
+    //obj->Translate(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
 
-	std::cout << "Information: " << std::endl;
-	std::cout << "\tGL Version: " << glGetString(GL_VERSION) << std::endl;
-	std::cout << "\tDisplay Address: " << display << std::endl;
-	std::cout << "\tRender Chain Address: " << renderChain << std::endl;
+    std::cout << "Information: " << std::endl;
+    std::cout << "\tGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "\tDisplay Address: " << display << std::endl;
+    std::cout << "\tRender Chain Address: " << renderChain << std::endl;
 
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-	while(!display->IsClosed())
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    while(!display->IsClosed())
     {
-		auto start = std::chrono::high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
 
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		renderChain->AttachRenderObject(obj.get());
-		renderChain->RenderObjectChain();
+        renderChain->AttachRenderObject(obj.get());
+        renderChain->RenderObjectChain();
 
         display->Update();
 
-		auto finish = std::chrono::high_resolution_clock::now();
+        auto finish = std::chrono::high_resolution_clock::now();
 
-		if(display->GetInputModule()->IsKeyPressed(Key_W))
-		{
-			display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, 0.1f));
-		}
-		if(display->GetInputModule()->IsKeyPressed(Key_S))
-		{
-			display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, -0.1f));
-		}
-		if(display->GetInputModule()->IsKeyPressed(Key_A))
-		{
-			display->GetCameraModule()->MoveCamera(glm::vec3(0.1f, 0.0f, 0.0f));
-		}
-		if(display->GetInputModule()->IsKeyPressed(Key_D))
-		{
-			display->GetCameraModule()->MoveCamera(glm::vec3(-0.1f, 0.0f, 0.0f));
-		}
-		
-		if(display->GetInputModule()->IsKeyPressed(Key_Space))
-		{
-			std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << std::endl;
-		}
+        if(display->GetInputModule()->IsKeyPressed(Key_W))
+        {
+            display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, 0.1f));
+        }
+        if(display->GetInputModule()->IsKeyPressed(Key_S))
+        {
+            display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, -0.1f));
+        }
+        if(display->GetInputModule()->IsKeyPressed(Key_A))
+        {
+            display->GetCameraModule()->MoveCamera(glm::vec3(0.1f, 0.0f, 0.0f));
+        }
+        if(display->GetInputModule()->IsKeyPressed(Key_D))
+        {
+            display->GetCameraModule()->MoveCamera(glm::vec3(-0.1f, 0.0f, 0.0f));
+        }
+
+        if(display->GetInputModule()->IsKeyPressed(Key_Space))
+        {
+            std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << std::endl;
+        }
 
     }
 
-	return 0;
+    return 0;
 }
+
+#pragma clang diagnostic pop
