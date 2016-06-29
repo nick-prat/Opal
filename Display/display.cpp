25e6acf666fa9631bc1af40ad021abf25ff64d47
Nick Prat <npissoawsome@gmail.com>
2016-06-28 20:10:57 -0400

    Required support for 3.3

--- Display/display.cpp
+++ Display/display.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+
 #include <GL/glew.h>
 #include <GL/gl.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "Utilities/utilities.hpp"
 #include "Display/display.h"
@@ -52,6 +55,9 @@ bool Display::InitDisplay(int width, int height, std::string title)
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	
     m_window = SDL_CreateWindow(title.c_str(), 0, 0, width, height, SDL_WINDOW_OPENGL);
 	
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <vector>
 #include <memory>
