13198e7d54d47d72a826b5cf9f8eafafac88f631
Nicholas Prat <npissoawsome@gmail.com>
2016-07-06 22:23:12 -0400

    Cleaing code

---
+++ OpenGL/openGL.cpp
@@ -0,0 +1,136 @@
+//
+// Created by nprat on 7/4/16.
+//
+
+#include <chrono>
+#include <string>
+
+#include "OpenGL.h"
+
+std::shared_ptr<OpenGL> OpenGL::m_openGL = nullptr;
+
+OpenGL::OpenGL()
+{
+    m_openGL = nullptr;
+    m_renderChain = nullptr;
+    m_shittyObject = nullptr;
+    m_display = nullptr;
+    m_lowestTime = 0;
+}
+
+OpenGL::~OpenGL()
+{}
+
+bool OpenGL::InitOpenGL(int width, int height, std::string title)
+{
+    std::cout << "Creating API" << std::endl;
+    gl::InitAPI();
+    std::cout << "Finished Creating API" << std::endl;
+
+    try
+    {
+        m_display = std::make_shared<GlutDisplay>(width, height, title);
+        m_renderChain = std::make_shared<RenderChain>(10);
+        m_shittyObject = std::make_shared<TestObject>(m_display);
+    }
+    catch (const char* error)
+    {
+        std::cout << "Error: " << error << std::endl;
+        return false;
+    }
+
+    //obj->Scale(glm::scale(glm::vec3(0.5f, 1.0f, 1.0f)));
+    //obj->Rotate(glm::rotate(glm::pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f)));
+    //obj->Translate(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+
+    std::cout << "Information: " << std::endl;
+    std::cout << "\tGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "\tDisplay Address: " << m_display << std::endl;
+    std::cout << "\tRender Chain Address: " << m_renderChain << std::endl;
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    return true;
+}
+
+void OpenGL::Destroy()
+{
+
+}
+
+void OpenGL::DisplayFunc()
+{
+    //std::cout << "DisplayFunc()" << std::endl;
+    //auto start = std::chrono::high_resolution_clock::now();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //std::cout << "OpenGL::DisplayFunc()" << std::endl;
+    m_renderChain->AttachRenderObject(m_shittyObject.get());
+    m_renderChain->RenderObjectChain();
+
+    glutSwapBuffers();
+
+    /*auto finish = std::chrono::high_resolution_clock::now();
+    
+    if(time < m_lowestTime || m_lowestTime == 0)
+    {
+        std::cout << "Fastest render : " << time << std::endl;
+        m_lowestTime = time;
+    }*/
+    /*
+    if(m_display->GetInputModule()->IsKeyPressed(Key_W))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, 0.1f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_S))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, -0.1f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_A))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.1f, 0.0f, 0.0f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_D))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(-0.1f, 0.0f, 0.0f));
+    }
+
+    if(m_display->GetInputModule()->IsKeyPressed(Key_Space))
+    {
+        std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << std::endl;
+    }
+     */
+}
+
+void OpenGL::DestroyInstance()
+{
+    m_openGL->Destroy();
+    m_openGL = nullptr;
+}
+
+bool OpenGL::CreateInstance(int width, int height, std::string title)
+{
+    if(m_openGL != nullptr)
+    {
+        std::cout << "OpenGL has already been created, destroy first";
+        return false;
+    }
+
+    m_openGL = std::make_shared<OpenGL>();
+    if(!m_openGL->InitOpenGL(width, height, title))
+    {
+        std::cout << "Couldn't initialize OpenGL project" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+std::shared_ptr<OpenGL> OpenGL::getInstance()
+{
+    return m_openGL;
+}
+
---
+++ OpenGL/openGL.h
@@ -0,0 +1,44 @@
+//
+// Created by nprat on 7/4/16.
+//
+
+#ifndef _OPENGL_OPENGL_H
+#define _OPENGL_OPENGL_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Model/renderchain.h>
+#include <Display/display.h>
+#include <Model/testObject.h>
+
+#include "glapi.h"
+
+class OpenGL
+{
+public:
+    OpenGL();
+    ~OpenGL();
+
+    void DisplayFunc();
+
+    static bool CreateInstance(int width, int height, std::string title);
+    static void DestroyInstance();
+    static std::shared_ptr<OpenGL> getInstance();
+
+private:
+    bool InitOpenGL(int width, int height, std::string title);
+    void Destroy();
+
+private:
+    static std::shared_ptr<OpenGL> m_openGL;
+
+private:
+    long m_lowestTime;
+    std::shared_ptr<GlutDisplay> m_display;
+    std::shared_ptr<RenderChain> m_renderChain;
+    std::shared_ptr<TestObject> m_shittyObject;
+};
+
+
+#endif //OPENGL_OPENGL_H
