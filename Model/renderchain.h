0e15bd4c1d362bdd98f27f2b320b3b500522be68
Nick Prat <npissoawsome@gmail.com>
2016-07-28 23:01:27 -0400

    Turning render object into singleton

--- Model/renderchain.h
+++ Model/renderchain.h
@@ -6,25 +6,35 @@
 class RenderChain
 {
 public:
-    RenderChain();
-	RenderChain(int num);
-	RenderChain(int num, bool vol);
-    ~RenderChain();
-		
-	bool InitRenderChain(int numObjects, bool vol);
-	bool InitRenderChain(int numObjects);
-	void Destroy();
+
+
+    bool InitRenderChain(int numObjects, bool vol);
+    bool InitRenderChain(int numObjects);
+    void Destroy();
 
     bool AttachRenderObject(RenderObject* object);
     void RenderObjectChain();
 
+    std::shared_ptr<RenderChain> getInstance();
+    bool createInstance();
+    bool createInstance(int num);
+    bool createInstance(int num, bool vol);
+    void deleteInstance();
 protected:
 
 private:
-	int m_objCount;
-	int m_objLimit;
-	bool m_volatile;
-	RenderObject** m_memPool;
+    RenderChain();
+    RenderChain(int num);
+    RenderChain(int num, bool vol);
+    ~RenderChain();
+
+private:
+    static std::shared_ptr<RenderChain> m_renderChain;
+
+    int m_objCount;
+    int m_objLimit;
+    bool m_volatile;
+    RenderObject** m_memPool;
 };
 
 #endif // _RENDERCHAIN_H
