7a1da1902e45c47e5bb3262bbd47d8c6f8fda485
Nicholas Prat <npissoawsome@gmail.com>
2016-08-10 01:27:35 -0400

    blah

---
+++ Core/glapi.cpp
@@ -0,0 +1,69 @@
+//
+// Created by nprat on 7/5/16.
+//
+
+#include <iostream>
+
+#include "glapi.h"
+
+namespace gl
+{
+    PFNGLGENBUFFERSPROC glGenBuffers = 0;
+    PFNGLVALIDATEPROGRAMPROC glValidateProgram = 0;
+    PFNGLUSEPROGRAMPROC glUseProgram = 0;
+    PFNGLCREATEPROGRAMPROC glCreateProgram = 0;
+    PFNGLCREATESHADERPROC glCreateShader = 0;
+    PFNGLSHADERSOURCEPROC glShaderSource = 0;
+    PFNGLCOMPILESHADERPROC glCompileShader = 0;
+    PFNGLGETSHADERIVPROC glGetShaderiv = 0;
+    PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog = 0;
+    PFNGLATTACHSHADERPROC glAttachShader = 0;
+    PFNGLLINKPROGRAMPROC glLinkProgram = 0;
+    PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog = 0;
+    PFNGLDETACHSHADERPROC glDetachShader = 0;
+    PFNGLDELETESHADERPROC glDeleteShader = 0;
+    PFNGLDELETEPROGRAMPROC glDeleteProgram = 0;
+    PFNGLBINDBUFFERPROC glBindBuffer = 0;
+    PFNGLBUFFERDATAPROC glBufferData = 0;
+    PFNGLDELETEBUFFERSPROC glDeleteBuffers = 0;
+    PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = 0;
+    PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv = 0;
+    PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray = 0;
+    PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer = 0;
+    PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray = 0;
+    PFNGLGENVERTEXARRAYSPROC glGenVertexArrays = 0;
+    PFNGLBINDVERTEXARRAYPROC glBindVertexArray = 0;
+    PFNGLDELETEVERTEXARRAYSPROC glDeleteVertexArrays = 0;
+    PFNGLBINDATTRIBLOCATIONPROC glBindAttribLocation = 0;
+
+    void InitAPI()
+    {
+        glGenBuffers = (PFNGLGENBUFFERSPROC)glXGetProcAddressARB((const GLubyte*)"glGenBuffers");
+        glValidateProgram = (PFNGLVALIDATEPROGRAMPROC)glXGetProcAddressARB((const GLubyte*)"glValidateProgram");
+        glUseProgram = (PFNGLUSEPROGRAMPROC)glXGetProcAddressARB((const GLubyte*)"glUseProgram");
+        glCreateProgram = (PFNGLCREATEPROGRAMPROC)glXGetProcAddressARB((const GLubyte*)"glCreateProgram");
+        glCreateShader = (PFNGLCREATESHADERPROC)glXGetProcAddressARB((const GLubyte*)"glCreateShader");
+        glShaderSource = (PFNGLSHADERSOURCEPROC)glXGetProcAddressARB((const GLubyte*)"glShaderSource");
+        glCompileShader = (PFNGLCOMPILESHADERPROC)glXGetProcAddressARB((const GLubyte*)"glCompileShader");
+        glGetShaderiv = (PFNGLGETSHADERIVPROC)glXGetProcAddressARB((const GLubyte*)"glGetShaderiv");
+        glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)glXGetProcAddressARB((const GLubyte*)"glGetShaderInfoLog");
+        glAttachShader = (PFNGLATTACHSHADERPROC)glXGetProcAddressARB((const GLubyte*)"glAttachShader");
+        glLinkProgram = (PFNGLLINKPROGRAMPROC)glXGetProcAddressARB((const GLubyte*)"glLinkProgram");
+        glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)glXGetProcAddressARB((const GLubyte*)"glGetProgramInfoLog");
+        glDetachShader = (PFNGLDETACHSHADERPROC)glXGetProcAddressARB((const GLubyte*)"glDetachShader");
+        glDeleteShader = (PFNGLDELETESHADERPROC)glXGetProcAddressARB((const GLubyte*)"glDeleteShader");
+        glDeleteProgram = (PFNGLDELETEPROGRAMPROC)glXGetProcAddressARB((const GLubyte*)"glDeleteProgram");
+        glBindBuffer = (PFNGLBINDBUFFERPROC)glXGetProcAddressARB((const GLubyte*)"glBindBuffer");
+        glBufferData = (PFNGLBUFFERDATAPROC)glXGetProcAddressARB((const GLubyte*)"glBufferData");
+        glDeleteBuffers = (PFNGLDELETEBUFFERSPROC)glXGetProcAddressARB((const GLubyte*)"glDeleteBuffers");
+        glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)glXGetProcAddressARB((const GLubyte*)"glGetUniformLocation");
+        glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)glXGetProcAddressARB((const GLubyte*)"glUniformMatrix4fv");
+        glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)glXGetProcAddressARB((const GLubyte*)"glEnableVertexAttribArray");
+        glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)glXGetProcAddressARB((const GLubyte*)"glVertexAttribPointer");
+        glDisableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC)glXGetProcAddressARB((const GLubyte*)"glDisableVertexAttribArray");
+        glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC)glXGetProcAddressARB((const GLubyte*)"glGenVertexArrays");
+        glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)glXGetProcAddressARB((const GLubyte*)"glBindVertexArray");
+        glDeleteVertexArrays = (PFNGLDELETEVERTEXARRAYSPROC)glXGetProcAddressARB((const GLubyte*)"glDeleteVertexArrays");
+        glBindAttribLocation = (PFNGLBINDATTRIBLOCATIONPROC)glXGetProcAddressARB((const GLubyte*)"glBindAttribLocation");
+    }
+}
---
+++ Core/openGL.cpp
@@ -0,0 +1,128 @@
+//
+// Created by nprat on 7/4/16.
+//
+
+#include <chrono>
+#include <string>
+#include <Model/Assimp/assimploader.h>
+
+#include "openGL.h"
+
+OpenGL* OpenGL::m_openGL = nullptr;
+
+OpenGL::OpenGL()
+{
+    m_openGL = nullptr;
+    m_obj = nullptr;
+    m_display = nullptr;
+    m_lowestTime = 0;
+}
+
+OpenGL::~OpenGL()
+{}
+
+bool OpenGL::InitOpenGL(int width, int height, std::string title)
+{
+    gl::InitAPI();
+
+    try
+    {
+        m_display = std::make_shared<GlutDisplay>(width, height, title);
+        m_obj = std::make_shared<TestObject>(m_display);
+        m_obj2 = std::make_shared<TestObject>(m_display);
+    }
+    catch (const char* error)
+    {
+        std::cout << "Error: " << error << std::endl;
+        return false;
+    }
+
+    m_obj->Translate(glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, -0.5f)));
+
+    std::cout << "Information: " << std::endl;
+    std::cout << "\tGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "\tDisplay Address: " << m_display << std::endl;
+    std::cout << "\tRender Chain Address: " << RenderChain::GetInstance() << std::endl;
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    return true;
+}
+
+void OpenGL::Destroy()
+{}
+
+void OpenGL::DisplayFunc()
+{
+    //auto start = std::chrono::high_resolution_clock::now();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    RenderChain* renderChain = RenderChain::GetInstance();
+    renderChain->AttachRenderObject(m_obj.get());
+    renderChain->AttachRenderObject(m_obj2.get());
+    renderChain->RenderObjectChain();
+    glutSwapBuffers();
+
+    /*auto finish = std::chrono::high_resolution_clock::now();
+    
+    if(time < m_lowestTime || m_lowestTime == 0)
+    {
+        std::cout << "Fastest render : " << time << std::endl;
+        m_lowestTime = time;
+    }*/
+
+    /*if(m_display->GetInputModule()->IsKeyPressed(Key_W))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, 0.1f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_S))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.0f, 0.0f, -0.1f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_A))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(0.1f, 0.0f, 0.0f));
+    }
+    if(m_display->GetInputModule()->IsKeyPressed(Key_D))
+    {
+        m_display->GetCameraModule()->MoveCamera(glm::vec3(-0.1f, 0.0f, 0.0f));
+    }
+
+    if(m_display->GetInputModule()->IsKeyPressed(Key_Space))
+    {
+        std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() << std::endl;
+    }*/
+}
+
+void OpenGL::DeleteInstance()
+{
+    m_openGL->Destroy();
+    delete m_openGL;
+    m_openGL = nullptr;
+}
+
+bool OpenGL::CreateInstance(int width, int height, std::string title)
+{
+    if(m_openGL != nullptr)
+    {
+        std::cout << "OpenGL has already been created, destroy first";
+        return false;
+    }
+
+    m_openGL = new OpenGL();
+    if(!m_openGL->InitOpenGL(width, height, title))
+    {
+        std::cout << "Couldn't initialize OpenGL project" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+OpenGL* OpenGL::GetInstance()
+{
+    return m_openGL;
+}
+
