e039d5e5d04de2b9c16a094d911086abee4a3b83
Nick Prat <npissoawsome@gmail.com>
2016-11-10 16:14:29 -0500

    including resources into git now

--- run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [[ $1 == "DEBUG" ]]; then
-	(cd ./bin/debug/ && exec ./OpenGL)
-elif [[ $1 == "RELEASE" ]]; then
-	(cd ./bin/release/ && exec ./OpenGL)
-else
-	echo "Unknown Build Type"
-fi
