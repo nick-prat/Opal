88aa507cf3bb9256f936106b325202c14e9f30c8
Nicholas Prat <npissoawsome@gmail.com>
2016-11-24 11:29:01 -0500

    added resources folder

---
+++ Resources/Shaders/staticmodel_fs.glsl
@@ -0,0 +1,14 @@
+#version 330
+
+in vec4 color;
+in vec2 texCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D gSampler;
+
+void main() {
+    //FragColor = color;
+    FragColor = texture2D(gSampler, texCoord);
+    //FragColor = mix(texture2D(gSampler, texCoord), color, 0.9);
+}
