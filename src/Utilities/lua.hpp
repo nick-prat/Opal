94c5bc778113e24f640460b9a18492cc59249020
Nicholas Prat <npissoawsome@gmail.com>
2017-02-18 04:21:46 -0500

    added lua helper file in Utilities/

---
+++ src/Utilities/lua.hpp
@@ -0,0 +1,48 @@
+#ifndef _LUA_H
+#define _LUA_H
+
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
+
+#include <LuaBridge/LuaBridge.h>
+
+// NOTE Helper class to make sure the lua state is closed last
+class LuaState {
+public:
+    LuaState() : m_lState(luaL_newstate()) {
+        luaL_openlibs(m_lState);
+    }
+
+    LuaState(const LuaState&) = delete;
+
+    LuaState(LuaState&& luaState) : m_lState(luaState.m_lState) {
+        luaState.m_lState = nullptr;
+    }
+
+    ~LuaState() {
+        if(m_lState != nullptr) {
+            lua_close(m_lState);
+            m_lState = nullptr;
+        }
+    }
+
+    LuaState& operator=(LuaState&) = delete;
+
+    inline LuaState& operator=(LuaState&& luaState) {
+        m_lState = luaState.m_lState;
+        luaState.m_lState = nullptr;
+        return *this;
+    }
+
+    inline lua_State* get() {
+        return m_lState;
+    }
+
+private:
+    lua_State* m_lState;
+};
+
+#endif // _LUA_H
