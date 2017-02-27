9aba1769851153e2832708639f098abe1ed6e38d
Nick Prat <npissoawsome@gmail.com>
2017-02-26 22:29:59 -0500

    working on removing old entity class

--- src/Entity/entity.hpp
+++ src/Entity/entity.hpp
@@ -207,30 +207,4 @@ private:
     std::array<std::unordered_map<unsigned int, unsigned int>, parameter_size> m_componentMaps;
 };
 
-class Entity {
-public:
-    Entity();
-    Entity(DynamicModel* model);
-    ~Entity();
-
-    void unbindModel();
-    void bindModel(DynamicModel* model);
-
-    void setVisible(const bool visible);
-    void setName(const std::string& name);
-
-    bool isVisible() const;
-    std::string getName() const;
-    DynamicModel* getModel() const;
-
-    void translate(const glm::vec3& trans);
-    void rotate(const float degrees, const glm::vec3& rotate);
-    void scale(const glm::vec3& scale);
-
-private:
-    glm::vec3 m_location;
-    std::string m_name;
-    DynamicModel* m_model;
-};
-
 #endif // _ENTITY_H
