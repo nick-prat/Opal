5d4d5778ab747ea6070ac5167cdeac55ab01c8fb
Nick Prat <npissoawsome@gmail.com>
2017-06-21 14:23:51 -0400

    Fixed broken input

--- src/Components/components.hh
+++ src/Components/components.hh
@@ -9,21 +9,27 @@
 #include <Resources/texture.hh>
 #include <Resources/model3d.hh>
 
-class CLocation {
+class CBody {
 public:
-    CLocation() = default;
-    CLocation(const glm::vec3& loc);
+    CBody() = default;
+    CBody(const glm::vec3& loc);
 
     void setLocation(const glm::vec3& loc);
     void setLocation(const glm::mat4& loc);
     const glm::mat4& getLocation() const;
 
-    void setDirection(const glm::vec3& dir);
-    glm::vec3 getDirection() const;
+    void setRotation(const glm::vec3& rotation);
+    void setRotation(const glm::mat4& rotation);
+    const glm::mat4& getRotation() const;
+
+    void setScale(const glm::vec3& scale);
+    void setScale(const glm::mat4& scale);
+    const glm::mat4& getScale() const;
 
 private:
+    glm::mat4 m_rotate;
+    glm::mat4 m_scale;
     glm::mat4 m_location;
-    glm::vec3 m_direction;
 };
 
 class CRender {
@@ -31,20 +37,12 @@ public:
     CRender() = default;
     CRender(const Model3D& model);
 
-    const glm::mat4& getRotation() const;
-    void setRotation(const glm::mat4& rotation);
-
-    const glm::mat4& getScale() const;
-    void setScale(const glm::mat4& scale);
-
     const std::vector<GLuint> getVAOs() const;
     const Model3D& getModel() const;
 
 private:
     const Model3D& m_model;
     std::vector<GLuint> m_vaos;
-    glm::mat4 m_rotate;
-    glm::mat4 m_scale;
 };
 
 #endif // _COMPONENTS_H
