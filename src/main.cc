eb80822cabca0ae1e6a5c5fb9a11039a636065b0
Nick Prat <npissoawsome@gmail.com>
2017-06-21 13:55:41 -0400

    input disabled

--- src/main.cc
+++ src/main.cc
@@ -11,13 +11,13 @@
 // TODO Ensure const correctness is follwed in whole project
 // TODO Implement some sort of collsion detection (bullet physics library?)
 
-int main(int argc, char **args) {
+int main(int argc, char* argv[]) {
     if(argc != 2) {
-        Log::getErrorLog() << "Arguments are invalid, use:\n\t" << args[0] << " [scene_name]\n";
+        Log::getErrorLog() << "Arguments are invalid, use:\n\t" << argv[0] << " [scene_name]\n";
         return -1;
     }
 
-    const std::string scenename = args[1];
+    const std::string scenename = argv[1];
     const std::string title = "OpenGL Project";
     constexpr int width = 1024;
     constexpr int height = 576;
@@ -35,19 +35,8 @@ int main(int argc, char **args) {
     glCore.setClearColor(glm::vec4(0.0f));
     glCore.setVsync(true);
 
-    Scene scene = glCore.createScene(scenename);
-    glCore.startScene(&scene);
-
-    double timer = glfwGetTime();
-    unsigned long frames = 0;
-
-    while(!glCore.shouldClose()) {
-        glCore.displayFunc();
-        frames++;
-    }
-
-    timer = glfwGetTime() - timer;
-    Log::getLog() << "Average FPS: " << frames / timer << '\n';
+    glCore.loadScene(scenename);
+    glCore.start();
 
     glCore.destroy();
     //GLCore::closeAPI();
