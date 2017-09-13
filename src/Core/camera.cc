4e90534c3b6268b210585377f8166fdb3442f613
Nick Prat <npissoawsome@gmail.com>
2017-09-12 23:57:04 -0400

    Fixed rotation clamp

--- src/Core/camera.cc
+++ src/Core/camera.cc
@@ -32,7 +32,7 @@ glm::mat4 Opal::Camera::getViewMatrix() const {
 // NOTE How far should the camera be able to move?
 void Opal::Camera::rotateCamera(const glm::vec3 rotation) {
     m_rotation += rotation;
-    glm::clamp(m_rotation.x, -0.9f, 0.9f);
+    glm::clamp(m_rotation.x, m_rotationClamp.x, m_rotationClamp.y);
     m_direction = glm::vec3(
             glm::sin(m_rotation.y) * glm::cos(m_rotation.x),
             -glm::sin(m_rotation.x),
